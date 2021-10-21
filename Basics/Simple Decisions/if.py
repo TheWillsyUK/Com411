3fb659752eb95d2e1e5b55cf82f13a10bd0feed6
Matt <4willm69a@solent.ac.uk>
2021-10-21 08:18:25 +0100

    All Git Work

--- Basics/Simple Decisions/if.py
+++ Basics/Simple Decisions/if.py
@@ -1,7 +1,22 @@
+# Task 1 - Week 2
 print("What type of book is this?")
 book_type = input()
 
 if book_type == "adventure":
     print("I like adventure books")
 
-    print("Finished reading gibook")
+    print("Finished reading book")
+
+# Tast 2 - Week 2
+
+print("Please enter the activity to be performed")
+activity = input()
+
+if activity == "calculate":
+    print("Performing calculations...")
+else:
+    print("Perfoming activity...")
+
+print("Activity Completed")
+
+# Task 3 - week 2
---
+++ Basics/Simple Decisions/if_else.py
@@ -0,0 +1,11 @@
+# Tast 2 - Week 2
+
+print("Please enter the activity to be performed")
+activity = input()
+
+if activity == "calculate":
+    print("Performing calculations...")
+else:
+    print("Perfoming activity...")
+
+print("Activity Completed")
